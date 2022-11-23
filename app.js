891497b8b7fc124c52212087082f0e8af0b658c4
Alon Mlievski <alonmlievski@gmail.com>
2022-11-23 20:51:34 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ var gGame = {
   secsPassed: 0,
 };
 
+var gTimer = false;
+
 function initGame() {
   gBoard = buildBoard();
-  console.log(gBoard);
   renderBoard(gBoard);
+  document.addEventListener('contextmenu', event => event.preventDefault());
 }
 
 function buildBoard() {
@@ -32,7 +34,7 @@ function buildBoard() {
         minesAroundCount: 0,
         isShown: false,
         isMine: false,
-        isMarked: true,
+        isMarked: false,
       };
     }
   }
@@ -65,7 +67,7 @@ function renderBoard(board) {
       } else {
         currCell = "";
       }
-      strHTML += `<td onClick="getCellLocation(this)" class="cellId-${i}-${j}">${currCell}</td>`;
+      strHTML += `<td onmousedown="getCellLocation(this)" oncontextmenu="alert('test')" class="cellId-${i}-${j}">${currCell}</td>`;
     }
     strHTML += "</tr>";
   }
@@ -74,24 +76,79 @@ function renderBoard(board) {
 }
 
 function getCellLocation(ev) {
-  console.log(ev.classList);
-  var rowIdx = +ev.classList[0].charAt("7");
-  var colIdx = +ev.classList[0].charAt("9");
+  var rowIdx = +ev.className.charAt("7");
+  var colIdx = +ev.className.charAt("9");
   var elCell = document.querySelector(`.cellId-${rowIdx}-${colIdx}`);
   cellClicked(elCell, rowIdx, colIdx);
 }
 
 function cellClicked(elCell, i, j) {
-  var elCell = document.querySelector(`.cellId-${i}-${j}`);
-  console.log(elCell, +i, +j);
-  if (elCell.innerText > 0) {
-    gBoard[i][j].isShown = true;
-    elCell.style.opacity = "100%";
-  }
-  console.log(gBoard);
+  //for Right Click
+
+//   if clicked with right side - add FLAG
+
+//   if clicked with left side - add num / bomb
+  console.log(elCell);
+    // addFlag();
 }
 
-function cellMarked(elCell) {}
+// function addFlag(e) {
+//     if (gBoard[i][j].isMarked === false && gBoard[i][j].isshown === false) {
+//         console.log(gBoard[i][j]);
+//         gBoard[i][j].isMarked = true;
+//         gGame.markedCount++
+//         console.log(gGame);
+//         elCell.innerText = FLAG
+//         elCell.style.opacity = "100%";
+//     }
+// }
+
+
+// function cellClicked(elCell, i, j) {
+//     for left
+//     function disableRightClick(e) {
+//         e.preventDefault();
+//         addFlag()
+//       }
+//   var elCell = document.querySelector(`.cellId-${i}-${j}`);
+//   elCell.addeventListener('click', function(e)) {
+//     click.elCell
+//   }
+//   console.log(elCell);
+//     gBoard[i][j].isShown = true;
+//     elCell.style.opacity = "100%";
+//   console.log(gBoard);
+// }
+
+// function addFlag(elCell, i, j) {
+//     var elCell = document.querySelector(`.cellId-${i}-${j}`);
+//     if
+// }
+
+// function getCellLocationRight(ev) {
+//     var rowIdx = +ev.className.charAt("7");
+//     var colIdx = +ev.className.charAt("9");
+//     var elCell = document.querySelector(`.cellId-${rowIdx}-${colIdx}`);
+//     cellClickedRight(elCell, rowIdx, colIdx);
+//   }
+
+//   function cellClickedRight(elCell, i, j) {
+//     var elCell = document.querySelector(`.cellId-${i}-${j}`);
+//     if (gBoard[i][j].isShown === false && gBoard[i][j].isMarked === false) {
+//        console.log(gBoard[i][j]);
+
+//     }
+//     console.log(gBoard);
+//   }
+
+function cellMarked(elCell) {
+  var elCell = document.querySelector(".cellId");
+  console.log(elCell);
+  // if (!gBoard.isShown && isMine)
+  //         isShown: false,
+  //     isMine: false,
+  //     isMarked: true,
+}
 
 function checkGameOver() {}
 
@@ -127,3 +184,26 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+function myTimer() {
+  var elTimer = document.querySelector(".timer span");
+  var time = elTimer.innerText;
+  time++;
+  elTimer.innerText = time;
+}
+
+function gameStart() {
+  gGame.isOn = true;
+  startTimer();
+  gTimer = true;
+}
+
+function startTimer() {
+  if (gTimer) return;
+  if (gGame.isOn) setInterval(myTimer, 1000);
+  else clearInterval;
+}
+
+// function disableRightClick(e) {
+//   e.preventDefault();
+// }
